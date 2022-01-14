2281b0429b8aeea7c21b8ba230b2ec33b28e92b5
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-01-14 16:46:14 +0100

    save

--- prototype/prototype-0/script.js
+++ prototype/prototype-0/script.js
@@ -3,10 +3,12 @@ var workManager = new WorkManager();
 var selectedRow = null;
 var rowId = null;
 
+
 document.getElementById("formSubmit").addEventListener("submit", function (event) {
     event.preventDefault();
     var work = readwork();
     workManager.addWork(work);
+
 
     insertNewRow();
 
@@ -18,6 +20,7 @@ function resetForm() {
     selectedRow = null;
 }
 
+
 function readwork() {
     var work = new Work();
 
@@ -25,13 +28,16 @@ function readwork() {
     return work;
 }
 
+
+
 function insertNewRow() {
-    var workList = workManager.getAllItems()
+    var workList = workManager.workList
     var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
 
     while (tableBody.rows.length > 0) {
         tableBody.deleteRow(0);
     }
+
 
     for (var i = 0; i < workList.length; i++) {
         var newRow = tableBody.insertRow(tableBody.length);
@@ -39,5 +45,21 @@ function insertNewRow() {
         cell2 = newRow.insertCell(1);
         cell2.innerHTML = workList[i].title;
         cell3 = newRow.insertCell(2)
+
+
+        var editButton = document.createElement("button")
+        var deleteButton = document.createElement("button")
+
+        var editContent = document.createTextNode("Edit")
+        editButton.appendChild(editContent)
+        editButton.setAttribute('onclick', 'onEdit(this)')
+
+        var deleteContent = document.createTextNode('Delete')
+        deleteButton.appendChild(deleteContent)
+        deleteButton.setAttribute("onclick", 'onDelete(this)')
+
+        cell3.appendChild(editButton)
+        cell3.appendChild(deleteButton)
     }
+
 }
